34c5ae090cdae1bfee2f074cd37734d057460a47
Richard White <r@rwhite.no>
2018-11-01 09:23:06 +0200

    initialize

---
+++ website/_000deploy.R
@@ -0,0 +1,3 @@
+bookdown::render_book('index.Rmd', 'bookdown::gitbook',output_dir="../docs/")
+bookdown::render_book('index.Rmd', 'bookdown::pdf_book',output_dir="../docs/")
+bookdown::render_book('index.Rmd', 'bookdown::epub_book',output_dir="../docs/")
