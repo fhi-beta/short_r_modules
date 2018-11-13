b2729285e2fbbfe50deb2dc556d1a0fbc8309a58
Beatriz Valcarcel <GryMaryson.Groneng@fhi.no>
2018-11-13 15:30:53 +0100

    change section name

--- website/_000deploy.R
+++ website/_000deploy.R
@@ -1,3 +1,4 @@
 bookdown::render_book('index.Rmd', 'bookdown::gitbook',output_dir="../docs/")
 bookdown::render_book('index.Rmd', 'bookdown::pdf_book',output_dir="../docs/")
 bookdown::render_book('index.Rmd', 'bookdown::epub_book',output_dir="../docs/")
+
